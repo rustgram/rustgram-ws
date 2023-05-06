9e8e52013c16cac4e52fe0ec82322be03cc7886e
Jörn Heinemann <joernheinemann@gmx.de>
2023-05-06 17:04:43 +0200

    added split example

--- examples/websockets/src/main.rs
+++ examples/websockets/src/main.rs
@@ -1,9 +1,11 @@
 use std::net::SocketAddr;
 
+use futures_util::stream::{SplitSink, SplitStream, StreamExt};
+use futures_util::SinkExt;
 use hyper::StatusCode;
 use rustgram::service::IntoResponse;
 use rustgram::{r, Request, Response, Router};
-use rustgram_ws::ws::WebSocket;
+use rustgram_ws::ws::{Message, WebSocket};
 
 async fn not_found_handler(_req: Request) -> Response
 {
@@ -19,6 +21,7 @@ async fn main()
 	let mut router = Router::new(not_found_handler);
 
 	router.get("/ws", r(ws_handler));
+	router.get("/ws_split", r(ws_handler_concurrently));
 
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
@@ -46,4 +49,36 @@ async fn handle(mut socket: WebSocket)
 			return;
 		}
 	}
+}
+
+async fn ws_handler_concurrently(req: Request) -> impl IntoResponse<Response>
+{
+	rustgram_ws::ws::on_upgrade(req, handle_concurrently)
+}
+
+async fn handle_concurrently(socket: WebSocket)
+{
+	let (sender, receiver) = socket.split();
+
+	tokio::spawn(write_socket(sender));
+	tokio::spawn(read_socket(receiver));
+}
+
+async fn read_socket(mut receiver: SplitStream<WebSocket>)
+{
+	while let Some(msg) = receiver.next().await {
+		let msg = if let Ok(msg) = msg {
+			msg
+		} else {
+			// client disconnected
+			return;
+		};
+
+		println!("got msg: {}", msg.to_text().unwrap());
+	}
+}
+
+async fn write_socket(mut sender: SplitSink<WebSocket, Message>)
+{
+	sender.send("hello world".into()).await.unwrap();
 }
